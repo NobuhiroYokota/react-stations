50df3afd3d8644d4a0777effb041971dfd1d151c
NobuhiroYokota <yokota.nobuhiro.job@gmail.com>
2024-03-28 12:52:32 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,23 @@ import { useEffect, useState } from 'react';
 import './App.css'
 import { logRoles } from '@testing-library/react';
 import { data } from 'browserslist';
+import Description from './Description';
+import Header from './Header';
+import DogImage from './DogImage';
+import DogListContainer from './DogListContainer';
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
 
-  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg');
-
-  const handleUrlChange = () => {
-      fetch('https://dog.ceo/api/breeds/image/random')
-      .then(response => {
-        return response.json();
-      }).then(data =>{
-        setDogUrl(data.message)
-      })
-    };
-
-
-
   return (
-    <header>
-      Dogアプリ
-      <div>
-        犬の画像を表示するサイトです
-      </div>
-      <img src={dogUrl}/>
-      <button onClick={handleUrlChange}>更新</button>
-    </header>
-  )
+
+    <section>
+      <Header />
+      <Description />
+      <DogListContainer />
+    </section>
+
+  );
 }
